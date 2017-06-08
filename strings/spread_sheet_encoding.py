3d56b8f009e02e00024033390b3f1b85ed292478
Binoy Prakash <prakash.binoy@gmail.com>
2017-06-07 18:11:38 -0700

    Better version with reduce

--- strings/spread_sheet_encoding.py
+++ strings/spread_sheet_encoding.py
@@ -1,9 +1,10 @@
-def encode_spread_sheet_column(input):
-    answer = 0
-    for index, char in enumerate(input[::-1]):
-        answer += (26 ** index) * (ord(char) - ord('A') + 1)
-    return answer
+from functools import reduce
+from operator import add
 
-r = encode_spread_sheet_column("AD")
-r = encode_spread_sheet_column("ZZ")
-print(r)
+
+def encode_spread_sheet_column(input):
+    return reduce(add, ((26 ** i) * (ord(c) - ord('A') + 1)
+                        for i, c in enumerate(input[::-1])))
+
+assert encode_spread_sheet_column("AD") == 30
+assert encode_spread_sheet_column("ZZ") == 702
